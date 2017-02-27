87d2034282aba79bc7c880f351ef44df048c10fc
Mike Hallai <mch81@zips.uakron.edu>
2017-02-27 16:31:54 -0500

    make script to seed db with admin account and random users

--- Gemfile
+++ Gemfile
@@ -9,6 +9,7 @@ end
 gem 'rails', '5.0.1'
 gem 'bootstrap-sass', '3.3.6'
 gem 'bcrypt',         '3.1.11'
+gem 'faker',          '1.6.6'
 # Use Puma as the app server
 gem 'puma', '3.4.0'
 # Use SCSS for stylesheets
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Create Admin account
+User.create!(username: "admin", admin: true, password: "foobar", 
+  password_confirmation: "foobar")
+
+0.times do |n|
+  name  = Faker::Name.last_name
+  password = "password123"
+  User.create!(username:  name, password: password, 
+    password_confirmation: password)
+end
