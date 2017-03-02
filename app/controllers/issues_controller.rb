2955e110dc3d480ce1ec50935174cc3e8add5998
Mike Hallai <mch81@zips.uakron.edu>
2017-03-01 19:08:10 -0500

    admin can edit issues

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -1,2 +1,25 @@
 class IssuesController < ApplicationController
+  def show
+    @issue = Issue.find(params[:id])
+  end
+
+  def edit
+    @issue = Issue.find(params[:id])
+  end
+  
+  def update
+    @issue = Issue.find(params[:id])
+    if @issue.update_attributes(issue_params)
+      redirect_to @issue
+    else
+      render 'edit'
+    end
+  end
+
+  private
+
+    def issue_params
+      params.require(:issue).permit(:title, :desc1, :desc2, :type)
+    end
+
 end
