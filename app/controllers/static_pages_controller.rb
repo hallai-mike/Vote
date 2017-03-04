572cab41da459596badd8abe2b2aac19709ea348
Mike Hallai <mch81@zips.uakron.edu>
2017-03-03 20:02:37 -0500

    user can view the ballot

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,10 +1,20 @@
 class StaticPagesController < ApplicationController
   def home
+    if logged_in? 
+      if current_user.admin?
+        @issue = Issue.new
+        @issues = Issue.paginate(page: params[:page])
+      else
+        @ballot = Issue.paginate(page: params[:page])
+        @options = Option.paginate(page: params[:page])
+      end
+    end 
+  end
+
+  def results
     if logged_in? && current_user.admin?
-      @issue = Issue.new
-      @issues = Issue.paginate(page: params[:page])
+      @results = Result.paginate(page: params[:page])
     end
-        
   end
 
   def help
